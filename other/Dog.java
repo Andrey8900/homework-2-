97ad204f376a98b80cc2282bd139a5cd4f9f4140
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-07-31 16:33:57 +0300

    добавили класс Dog

--- other/Dog.java
+++ other/Dog.java
@@ -1,2 +1,36 @@
-package other;public class Dog {
+package other;
+
+import java.io.Serializable;
+import model.Identifiable;
+
+public class Dog implements Identifiable, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String breed;
+
+    public Dog(int id, String name, String breed) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{id=" + id + ", name='" + name + '\'' + ", breed='" + breed + '\'' + '}';
+    }
 }
