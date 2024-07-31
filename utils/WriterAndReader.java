9a570659fd98154b797a369831948a4209a16274
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-07-31 20:14:19 +0300

    изменён  utils/WriterAndReader.java

--- utils/WriterAndReader.java
+++ utils/WriterAndReader.java
@@ -1,11 +1,13 @@
 package utils;
 
 import service.FamilyTree;
+import model.Identifiable;
+
 import java.io.*;
 
 public class WriterAndReader {
 
-    public void writeToFile(String filename, FamilyTree familyTree) {
+    public <T extends Identifiable & Serializable> void writeToFile(String filename, FamilyTree<T> familyTree) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
             oos.writeObject(familyTree);
         } catch (IOException e) {
@@ -13,9 +15,9 @@ public class WriterAndReader {
         }
     }
 
-    public FamilyTree readFromFile(String filename) {
+    public <T extends Identifiable & Serializable> FamilyTree<T> readFromFile(String filename) {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            return (FamilyTree) ois.readObject();
+            return (FamilyTree<T>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
