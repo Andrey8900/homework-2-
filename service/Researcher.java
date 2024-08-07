53e56fe4aa08b2f9dae014e70fd1f2ec1c828154
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-07 14:44:10 +0300

    изменен  service/Researcher.java

--- service/Researcher.java
+++ service/Researcher.java
@@ -1,21 +1,22 @@
 package service;
 
-import model.Person;
+import model.Identifiable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Researcher {
-    public List<Person> getDescendants(Person person) {
+public class Researcher<T extends Identifiable> {
+    public List<T> getDescendants(T person) {
         return collectDescendants(person);
     }
 
-    private List<Person> collectDescendants(Person person) {
-        List<Person> descendants = new ArrayList<>();
+    private List<T> collectDescendants(T person) {
+        List<T> descendants = new ArrayList<>();
         if (person != null) {
-            for (Person child : person.getChildren()) {
-                descendants.add(child);
-                descendants.addAll(collectDescendants(child));
+            for (Identifiable child : person.getChildren()) {
+                T childAsT = (T) child; // Преобразование типа
+                descendants.add(childAsT);
+                descendants.addAll(collectDescendants(childAsT));
             }
         }
         return descendants;
