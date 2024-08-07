6b788fac65b404d0f4f00aebd5d1f6e86b8c29c9
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-07 14:41:42 +0300

    изменен model/Identifiable.java

--- model/Identifiable.java
+++ model/Identifiable.java
@@ -1,5 +1,16 @@
 package model;
 
-public interface Identifiable {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public interface Identifiable extends Serializable {
     int getId();
+    void addChild(Identifiable child);
+    void setFather(Identifiable father);
+    void setMother(Identifiable mother);
+    Gender getGender();
+    String getName();
+    Date getBirthDate();
+    List<? extends Identifiable> getChildren();
 }
