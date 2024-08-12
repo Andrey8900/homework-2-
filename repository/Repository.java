bf52f8963d0c0726b7be0e18f09c96858e5057cc
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-12 20:08:47 +0300

    изменён  repository/Repository.java

--- repository/Repository.java
+++ repository/Repository.java
@@ -1,2 +1,11 @@
-package repository;public class Repository {
+package repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    void add(T item);
+    T find(int id);
+    List<T> findAll();
+    void save(String filename);
+    void load(String filename);
 }
