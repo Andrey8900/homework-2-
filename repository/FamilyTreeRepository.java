7eab64ed6401738bb6442bea7e0b3e1599ea62ff
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-24 18:37:54 +0300

    изменён  repository/FamilyTreeRepository.java

--- repository/FamilyTreeRepository.java
+++ repository/FamilyTreeRepository.java
@@ -1,50 +1,21 @@
 package repository;
 
 import model.Person;
-import service.WriterAndReader;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FamilyTreeRepository implements Repository<Person> {
-    private List<Person> members;
-    private WriterAndReader writerAndReader;
+public class FamilyTreeRepository implements Repository {
+    private List<Person> people;
 
     public FamilyTreeRepository() {
-        this.members = new ArrayList<>();
-        this.writerAndReader = new WriterAndReader();
+        this.people = new ArrayList<>();
     }
 
     @Override
-    public void add(Person person) {
-        members.add(person);
+    public void save(Person person) {
+        people.add(person);
     }
 
-    @Override
-    public Person find(int id) {
-        for (Person member : members) {
-            if (member.getId() == id) {
-                return member;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public List<Person> findAll() {
-        return members;
-    }
-
-    @Override
-    public void save(String filename) {
-        writerAndReader.writeToFile(filename, members);
-    }
-
-    @Override
-    public void load(String filename) {
-        List<Person> loadedMembers = writerAndReader.readFromFile(filename);
-        if (loadedMembers != null) {
-            members = loadedMembers;
-        }
-    }
+    // Другие методы для работы с хранилищем
 }
