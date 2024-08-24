a3dfeed78f7c55f92d65300470ffebbe1e8882ca
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-24 18:39:15 +0300

    изменён   service/PersonService.java

--- service/PersonService.java
+++ service/PersonService.java
@@ -1,2 +1,12 @@
-package service;public class PersonService {
+package service;
+
+import model.Person;
+import model.Gender;
+
+import java.util.Date;
+
+public class PersonService {
+    public Person createPerson(int id, String name, Date birthDate, Gender gender) {
+        return new Person(id, name, birthDate, gender);
+    }
 }
