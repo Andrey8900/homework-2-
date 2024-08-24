e6aa4a0c843be671fce9f5c5089b29748d94d411
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-24 18:36:52 +0300

    изменён main/Main.java

--- main/Main.java
+++ main/Main.java
@@ -2,13 +2,16 @@ package main;
 
 import presenter.FamilyTreePresenter;
 import repository.FamilyTreeRepository;
+import repository.Repository;
 import service.FamilyTreeService;
+import service.PersonService;
 import view.ConsoleView;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTreeRepository repository = new FamilyTreeRepository();
-        FamilyTreeService service = new FamilyTreeService(repository);
+        Repository repository = new FamilyTreeRepository();
+        PersonService personService = new PersonService();
+        FamilyTreeService service = new FamilyTreeService(repository, personService);
         FamilyTreePresenter presenter = new FamilyTreePresenter(service);
         ConsoleView view = new ConsoleView(presenter);
         view.start();
