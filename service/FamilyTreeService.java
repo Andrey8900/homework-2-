f4e7e1d6be876504806736774edc91b23d988c49
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-08-24 18:38:42 +0300

    изменён   service/FamilyTreeService.java

--- service/FamilyTreeService.java
+++ service/FamilyTreeService.java
@@ -1,77 +1,24 @@
 package service;
 
-import model.Gender;
 import model.Person;
-import repository.FamilyTreeRepository;
+import model.Gender;
+import repository.Repository;
 
-import java.util.List;
+import java.util.Date;
 
 public class FamilyTreeService {
-    private FamilyTreeRepository repository;
-    private Researcher<Person> researcher;
+    private Repository repository;
+    private PersonService personService;
 
-    public FamilyTreeService(FamilyTreeRepository repository) {
+    public FamilyTreeService(Repository repository, PersonService personService) {
         this.repository = repository;
-        this.researcher = new Researcher<>();
+        this.personService = personService;
     }
 
-    public void addFamilyMember(Person person) {
-        repository.add(person);
+    public void addPerson(int id, String name, Date birthDate, Gender gender) {
+        Person person = personService.createPerson(id, name, birthDate, gender);
+        repository.save(person);
     }
 
-    public boolean setParentForChild(int parentId, int childId) {
-        Person parent = repository.find(parentId);
-        Person child = repository.find(childId);
-
-        if (parent != null && child != null) {
-            parent.addChild(child);
-            if (parent.getGender() == Gender.MALE) {
-                child.setFather(parent);
-            } else if (parent.getGender() == Gender.FEMALE) {
-                child.setMother(parent);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public void sortByName() {
-        List<Person> members = repository.findAll();
-        members.sort((p1, p2) -> p1.getName().compareTo(p2.getName()));
-    }
-
-    public void sortByBirthDate() {
-        List<Person> members = repository.findAll();
-        members.sort((p1, p2) -> p1.getBirthDate().compareTo(p2.getBirthDate()));
-    }
-
-    public List<Person> getFamilyMembers() {
-        return repository.findAll();
-    }
-
-    public boolean saveFamilyTree(String filename) {
-        try {
-            repository.save(filename);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean loadFamilyTree(String filename) {
-        try {
-            repository.load(filename);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public List<Person> getDescendants(int personId) {
-        Person person = repository.find(personId);
-        if (person != null) {
-            return researcher.getDescendants(person);
-        }
-        return null;
-    }
+    // Другие методы работы с семейным деревом
 }
